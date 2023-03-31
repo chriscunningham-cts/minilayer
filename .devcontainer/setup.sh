0b44ffc29b201c585663fbdccaca7de05923768f
chriscunningham-cts <chris.cunningham@cloudsolutions.co.uk>
2023-03-31 14:15:49 +0000

    postStartCommand working now

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
-npm install -g @devcontainers/cli
+sudo apt-get update && sudo apt-get -y install qemu binfmt-support qemu-user-static npm
 
-pip install poetry
+sudo npm install -g @devcontainers/cli
+
+pip install --user poetry
 
 poetry install --with dev,generate
-
-apt-get -y install qemu binfmt-support qemu-user-static
 
 docker run --rm --privileged multiarch/qemu-user-static:latest --reset --credential yes -p yes
